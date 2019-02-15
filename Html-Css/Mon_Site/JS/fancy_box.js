c92e51955a39a4c7edbba4741b720a64d001a1a7
toimi68 <45996495+toimi68@users.noreply.github.com>
2019-02-15 16:56:09 +0100

    15-02-19

---
+++ Html-Css/Mon_Site/JS/fancy_box.js
@@ -0,0 +1,9 @@
+$('[data-fancybox]').fancybox({
+
+    loop: true,
+
+    buttons: ["zoom", "share", "slideShow", "fullScreen", "download", "thumbs", "close"],
+
+    animationEffect: "zoom-in-out"
+
+});
